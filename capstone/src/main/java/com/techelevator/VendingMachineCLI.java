7405542795f1748e3de7450d446ce6b6e5d84ae7
Gabriel Hernandez <hernandezgabriel533@gmail.com>
2022-04-16 20:15:21 +0000

    Revert "Revert "Changed passing objects through methods to static for clearer functionality""
    This reverts commit 18f0a35be5a29f40883a6794f6551fcbf3809f32

--- capstone/src/main/java/com/techelevator/VendingMachineCLI.java
+++ capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -3,13 +3,15 @@ package com.techelevator;
 import com.techelevator.view.Menu;
 import com.techelevator.view.VendingMachine;
 
+
 public class VendingMachineCLI {
 
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
 	private static final String MAIN_MENU_OPTION_EXIT = "Exit";
+	private static final String MAIN_MENU_OPTION_HIDDEN= "";
 	private static final String[] MAIN_MENU_OPTIONS =
-			{ MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_EXIT };
+			{ MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_EXIT,MAIN_MENU_OPTION_HIDDEN} ;
 
 	private Menu menu;
 
@@ -17,7 +19,7 @@ public class VendingMachineCLI {
 		this.menu = menu;
 	}
 
-	public void run(VendingMachine vendingMachine) {
+	public void run() {
 
 		boolean keepRunning=true;
 		while (keepRunning) {
@@ -25,15 +27,20 @@ public class VendingMachineCLI {
 
 			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
 
-				vendingMachine.printInventory();
+				VendingMachine.printInventory();
 
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
-
-				vendingMachine.runMoneyHandler(vendingMachine);
+				//jumps to MoneyHandler class
+				VendingMachine.runMoneyHandler();
 
 			} else if (choice.equals(MAIN_MENU_OPTION_EXIT)){
 
 				keepRunning=false;
+
+			} else if (choice.equals(MAIN_MENU_OPTION_HIDDEN)){
+
+				System.out.println("Sales Report Selected");
+				VendingMachine.generateSalesLog();
 
 			}
 		}
--- capstone/src/main/java/com/techelevator/view/VendingMachineSnack.java
+++ capstone/src/main/java/com/techelevator/view/VendingMachineSnack.java
@@ -6,32 +6,53 @@ public class VendingMachineSnack {
     private String identifier;
     private String snackType;
     private int beginningInventory=5;
+    private int amountBought=0;
+
+    public int getAmountBought() {
+        return amountBought;
+    }
 
     public String getName() {
         return name;
     }
 
-    public VendingMachineSnack
-            (String identifier, String name, double price, String snackType) {
+    public VendingMachineSnack(String identifier, String name, double price, String snackType) {
+
         this.name = name;
         this.price = price;
         this.identifier = identifier;
         this.snackType=snackType;
+
     }
 
     @Override
     public String toString() {
+
         return identifier+" "+ name +" " + price +" "+snackType+" "+ beginningInventory;
+
+    }
+
+
+    public String toStringForSalesReport(){
+
+        return name+ " | " + amountBought + "\n";
+
     }
 
     public String getIdentifier() {
         return this.identifier;
     }
+
     public int getInventory(){
         return this.beginningInventory;
     }
+
     public void subtractInventory(){
         this.beginningInventory--;
+    }
+
+    public void addToAmountBought(){
+        amountBought++;
     }
 
     public double getPrice() {
@@ -39,6 +60,7 @@ public class VendingMachineSnack {
     }
 
     public String dispenseMessage(){
+
         if (snackType.equals("Candy")){
             return "Munch Munch, Yum!";
 
@@ -53,8 +75,11 @@ public class VendingMachineSnack {
         if(snackType.equals("Gum")) {
             return "Chew Chew, Yum!";
         }
+
         return "";
+
     }
+
 
 }
 
